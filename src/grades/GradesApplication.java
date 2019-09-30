c5d67e75a80ececc4030ff39cede43d26d56471c
Annette Iglesias <iglesiasannette@gmail.com>
2019-09-30 15:53:32 -0500

    complete grades application

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -1,58 +1,138 @@
 package grades;
 
-import java.util.ArrayList;
+import java.util.Scanner;
 import java.util.HashMap;
-import java.util.Map;
 
 
-public class GradesApplication {
-//    Create a map for students and GitHub usernames
+//    2. Create a map for students and GitHub usernames
 //
-//    Create a class named GradesApplication with a main method. Inside the main method, create a HashMap named students. It should have keys that are strings that represent github usernames, and values that are Student objects. Create at least 4 student objects with at least 3 grades each, and add them to the map.
+//    Create a class named GradesApplication with a main method. Inside the main method,
+//    create a HashMap named students. It should have keys that are strings that represent
+//    github usernames, and values that are Student objects. Create at least 4 student objects with at least 3 grades each, and add them to the map.
 //    Be creative! Make up GitHub usernames and grades for your student objects.
-    public static void main(String[] args) {
-        HashMap<String, Student> students = new HashMap<>();
 
-        Student annette = new Student("Annette");
-        annette.addGrade(90);
-        annette.addGrade(85);
-        annette.addGrade(40);
-
-        Student bridget = new Student ("Bridget");
-        bridget.addGrade(90);
-        bridget.addGrade(70);
-        bridget.addGrade(60);
-
-
-        Student bev = new Student("Bev");
-        annette.addGrade(97);
-        annette.addGrade(96);
-        annette.addGrade(95);
+//   3. Create the command line interface
+//
+//Print the list of GitHub usernames out to the console, and ask the user which student they would
+// like to see more information about. The user should enter a GitHub username (i.e. one of the keys
+// in your map). If the given input does not match up with a key in your map, tell the user that no
+// users with that username were found. If the given username does exist, display information about
+// that student, including their name and their grades.
+//
+//After the information is displayed, the application should ask the user if they want to continue,
+// and keep running so long as the answer is yes.
 
 
-        Student glo = new Student("Glo");
-        annette.addGrade(94);
-        annette.addGrade(93);
-        annette.addGrade(92);
+    public class GradesApplication {
+        static HashMap<String, Student> students;
+        // creating a new instance of a collection ,from the Student class, called students.
+        //the String parameter is the username we are going to be assigning, and Student is the data type of the new object.
 
 
+        private static void init() {
 
-        students.put("bmills", bridget);
-        students.put("annette1", annette);
-        students.put("bevvybev", bev);
-        students.put("thegloUp", glo);
+            students = new HashMap<String, Student>(); //redundant but there for my reference
 
-            String nameList = " | ";
+            //new Student objects, from the Student class, that are in our new HashMap called students
+            Student annette = new Student("Annette");
+            annette.addGrade(90); // addGrade method from the Student class
+            annette.addGrade(85);
+            annette.addGrade(40);
 
-        for(String username : students.keySet()){
-            nameList += " " + username + " | ";
+            Student bridget = new Student("Bridget");
+            bridget.addGrade(90);
+            bridget.addGrade(70);
+            bridget.addGrade(60);
+
+
+            Student bev = new Student("Bev");
+            bev.addGrade(97);
+            bev.addGrade(96);
+            bev.addGrade(95);
+
+
+            Student glo = new Student("Glo");
+            glo.addGrade(94);
+            glo.addGrade(93);
+            glo.addGrade(92);
+
+
+            // sets the usernames as keys
+            // and values that are student objects
+            students.put("bmills", bridget);
+            students.put("annette1", annette);
+            students.put("bevvybev", bev);
+            students.put("thegloUp", glo);
 
         }
-            System.out.println(nameList);
+
+            // enhanced-for loop that iterates through the hashmap and prints the key (which is the username,
+            // in this case) out to the console
+
+           public static void userDisplay() {
+
+               String nameList = " | ";
+
+               for (String username : students.keySet()) {
+                   nameList += " " + username + " | ";
+
+               }
+               System.out.println(nameList);
+
+           }
+
+
+        ///Print the list of GitHub usernames out to the console, and ask the user which student they would
+        //// like to see more information about. The user should enter a GitHub username (i.e. one of the keys
+        //// in your map). If the given input does not match up with a key in your map, tell the user that no
+        //// users with that username were found. If the given username does exist, display information about
+        //// that student, including their name and their grades
+
+
+            public static void userSelection (String key) {
+                // compare userInput with usernames
+                if (students.containsKey(key)) {
+                    System.out.printf("Name: %s - Github Username: %s%n", students.get(key).getName(), key);
+                    System.out.printf("Current Average: %.2f%n%n", students.get(key).getGradeAverage());
+                    //System.out.printf("Student grades are: %s", students.get(key).addGrade());
+                    // output student record
+                } else {
+                    System.out.printf("Sorry, no student found with the github username of \"%s\".%n", key);
+                }
+            }
 
 
 
 
-    }
+            public static void main (String[] args){
+            Scanner userInput = new Scanner (System.in);
 
-}
+
+            init();
+            System.out.println("Hello there! Here is a list of our users: \n");
+            userDisplay();
+            boolean keepGoing;
+
+            do {
+                System.out.println("\nWhich student would you like to see more information about? (Enter a Github username) : \n");
+                String userChoice = userInput.nextLine();
+                userSelection(userChoice);
+
+                System.out.println("Would you like to see more about another student? \n");
+                String userContinue = userInput.nextLine();
+
+                if (userContinue.equalsIgnoreCase("yes") || userContinue.equalsIgnoreCase("y")){
+                    keepGoing = true;
+                } else {
+                    System.out.println("Okay, bye!");
+                    keepGoing = false;
+                }
+
+            } while (keepGoing);
+
+
+
+            }
+
+        }
+
